f1959fd3e8eba42483c836fcefe95518b04019b6
Chris <christo_rivi@hotmail.com>
2026-03-23 08:54:22 -0600

    Configuracion de JSON P1

--- conexion.php
+++ conexion.php
@@ -5,21 +5,32 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 class Conexion extends PDO
 {
-    private $host='127.0.0.1:3308';
-    private $nombreBD='proyecto';
-    private $usuario='root';
-    private $contraseña='root';
+    private $host;
+    private $nombreBD;
+    private $usuario;
+    private $contraseña;
 
     public function __construct()
     {
         try
         {
-            parent::__construct('mysql:host='  .  $this->host . ';dbname=' . $this->nombreBD . ';charset=utf8',
-            $this->usuario,  $this->contraseña, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $config = json_decode(file_get_contents("config.json"), true);
+
+            $this->host = $config['database']['host'];
+            $this->nombreBD = $config['database']['dbname'];
+            $this->usuario = $config['database']['user'];
+            $this->contraseña = $config['database']['password'];
+
+            parent::__construct(
+                'mysql:host=' . $this->host . ';dbname=' . $this->nombreBD . ';charset=utf8',
+                $this->usuario,
+                $this->contraseña,
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+            );
         }
         catch(PDOException $e)
         {
-            echo 'Error: '  .  $e->getMessage();
+            echo 'Error: ' . $e->getMessage();
             exit;
         }
     }
